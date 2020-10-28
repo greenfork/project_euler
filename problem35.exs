ff203b8d3b2a9f337bf80657ef047f64316a07e5
Dmitry Matveyev <dm.matveyev@protonmail.com>
2020-10-28 21:13:25 +0300

    Solve problem 35

---
+++ problem35.exs
@@ -0,0 +1,73 @@
+defmodule CircularPrimes do
+  def run do
+    Enum.count(circular_primes())
+  end
+
+  def circular_primes do
+    primes = Eratosthenes.primes()
+
+    for {p, i} <- Enum.with_index(:array.to_list(primes)),
+        p == true,
+        circular_prime?(i, primes),
+        do: i
+  end
+
+  def circular_prime?(n, primes), do: Enum.all?(rotations(n), fn x -> aget(primes, x) end)
+
+  def rotate([a | b]), do: b ++ [a]
+
+  def rotations(n) do
+    digits = Integer.digits(n)
+    [_ | rotations] = Enum.reduce(digits, [digits], fn _, [h | _] = acc -> [rotate(h) | acc] end)
+    Enum.map(rotations, &Integer.undigits/1)
+  end
+
+  def aset(array, index, value), do: :array.set(index, value, array)
+  def aget(array, index), do: :array.get(index, array)
+end
+
+defmodule Eratosthenes do
+  @prime_limit 1_000_000
+
+  def prime_list(primes) do
+    for {p, i} <- Enum.with_index(:array.to_list(primes)), p == true, do: i
+  end
+
+  def primes do
+    arr =
+      :array.new(@prime_limit, {:default, true})
+      |> aset(0, false)
+      |> aset(1, false)
+
+    do_primes(arr, next_prime(arr, 1))
+  end
+
+  defp do_primes(array, prime) when is_nil(prime), do: array
+
+  defp do_primes(array, prime) do
+    array = crossout(array, prime)
+    do_primes(array, next_prime(array, prime))
+  end
+
+  defp crossout(array, prime) do
+    if prime * prime < @prime_limit do
+      Enum.reduce(:lists.seq(prime * prime, @prime_limit - 1, prime), array, fn p, acc ->
+        aset(acc, p, false)
+      end)
+    else
+      array
+    end
+  end
+
+  def next_prime(array, after_number) do
+    Enum.find((after_number + 1)..(@prime_limit - 1), fn x -> aget(array, x) == true end)
+  end
+
+  def aset(array, index, value), do: :array.set(index, value, array)
+  def aget(array, index), do: :array.get(index, array)
+end
+
+# IO.inspect(CircularPrimes.circular_primes(), limit: :infinity, pretty: true)
+IO.inspect(CircularPrimes.run())
+
+# 197, 971, 719 - rotated numbers and all are primes
